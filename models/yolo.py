6f932d823484df704f8e04814328558143e92271
Johnny Chang <50794318+Chen-MingChang@users.noreply.github.com>
2021-02-25 04:18:28 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -218,7 +218,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, ConvPlus, CrossConv, BottleneckCSP, BottleneckCSP2, SPPCSP, VoVCSP, C3]:
+        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, BottleneckCSP2, SPPCSP, VoVCSP, C3]:
             c1, c2 = ch[f], args[0]
 
             # Normal
