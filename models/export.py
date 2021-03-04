a5d06a80c67842cb5ae81b0a595a1e6cbe581d12
Johnny Chang <50794318+Chen-MingChang@users.noreply.github.com>
2021-03-05 01:14:09 +0800

    Update export.py

--- models/export.py
+++ models/export.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 
         print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
         f = opt.weights.replace('.pt', '.onnx')  # filename
-        torch.onnx.export(model, img, f, verbose=False, opset_version=10, input_names=['images'],
+        torch.onnx.export(model, img, f, verbose=False, opset_version=12, input_names=['images'],
                           output_names=['output'])
 
         # Checks
